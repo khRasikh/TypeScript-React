0b7c7f3f3b9a8667601becf3cf68cb9edeab3524
“khRasikh” <kh.rasikh542@gmail.com>
2022-03-29 13:46:21 +0300

    UseState Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import PassToAnother from "./Components/Propss";
 import PropsApp from "./Components/Propss";
+import UseState from "./Components/UseState";
 
 import {
   BrowserRouter as Routers,
@@ -16,10 +17,42 @@ export function Product() {
     <>
       <div className="min-h-full lg:text-left text-gray-600 p-8 bg-gray-700">
         <h3>Navigate to MasterFronentd lessons: </h3>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded">
           <Link to="/propsa">Props in React</Link>
-          <Link to="/propsb">Propsb in React</Link>
         </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+          <Link to="/propsb">Props Example</Link>
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+          <Link to="/usestate">Props Example</Link>
+        </button>
+      </div>
+      <div className="min-h-full lg:text-left text-gray-600 p-8 bg-gray-700 grid grid-cols-3 gap-4 content-start ...">
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+            <Link to="/usestate">Props Example</Link>
+          </button>
+        </div>
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+            <Link to="/usestate">Props Example</Link>
+          </button>
+        </div>
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+            <Link to="/usestate">Props Example</Link>
+          </button>
+        </div>
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+            <Link to="/usestate">Props Example</Link>
+          </button>
+        </div>
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mx-5 px-3 rounded">
+            <Link to="/usestate">Props Example</Link>
+          </button>
+        </div>
       </div>
     </>
   );
@@ -30,15 +63,12 @@ function App() {
     <>
       <Routers>
         <Header />
-        {/* <div className="min-h-full lg:text-left text-gray-600 p-8 bg-gray-700">
-          Welcome to TypeScript-React front style using Tailwind
-          <Link to="/">First</Link>
-        </div> */}
         <Routes>
           <Route path="/" element={<Product />} />
           <Route path="/footer" element={<Footer />} />
           <Route path="/propsa" element={<PassToAnother />} />
           <Route path="/propsb" element={<PropsApp />} />
+          <Route path="/usestate" element={<UseState />} />
         </Routes>
         <Footer />
       </Routers>
---
+++ src/Components/UseState.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Components.css";
+/**A very simple example of useState in TypeScript React */
+//1. import useEffect
+import { useState } from "react";
+
+/**Here is another example of useState in TypeScript React */
+const UseStateType = () => {
+  const [Odd, setOdd] = useState(0);
+  return (
+    <>
+      <div>
+        <div className="px-5 mx-6 my-6">
+          <p className="my-6">
+            <b>TypeScrit-React: </b>Here you see how I used UseState Example:
+          </p>
+          <h2 style={{ color: "green" }}></h2>
+          <button
+            type="button"
+            className="newButton"
+            onClick={() => setOdd(Odd + 1)}
+          >
+            Click Here to Increement(++)
+          </button>
+          <h2>{Odd}</h2>
+
+          <button
+            type="button"
+            className="newButton"
+            onClick={() => setOdd(Odd - 1)}
+          >
+            Click here to Decremeant(--)
+          </button>
+        </div>
+        <div className="newBody"></div>
+      </div>
+    </>
+  );
+};
+
+// export default UseEffectType;
+export default UseStateType;
